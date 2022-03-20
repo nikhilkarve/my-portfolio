16edb2ff105487b285a74fa8df39b12e724a9504
Nikhil Karve <nikhilkarve@Nikhils-MacBook-Air.local>
2022-03-20 17:53:00 -0400

    Homepage done.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+// import profilePicture from './assets/img/profile.jpeg';
+import profileNikhil from './assets/img/nikhil_image.jpg';
+import classes from './App.module.css';
+import { BsGithub, BsLinkedin, BsMedium } from 'react-icons/bs';
+import { SiGmail } from 'react-icons/si';
+import { useState } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div>
+      <div className={classes.profile}>
+        <img src={profileNikhil} alt="nikhil karve"></img>
+        <h1 className={classes.name}>Nikhil Karve</h1>
+        <p className={classes.tag}>Software Developer</p>
+      </div>
+      <div className={classes.profileButtons}>
+        <button>About</button>
+        <button className={classes.middle}>Projects</button>
+        <button>Blog</button>
+      </div>
+      <div className={classes.socialLinks}>
+        <a href="https://www.github.com/nikhilkarve">
+          <BsGithub className={classes.icons} />
         </a>
-      </header>
+        <a href="https://www.linkedin.com/in/nikhil-karve-66067a183/">
+          <BsLinkedin className={classes.icons} />
+        </a>
+        <a href="">
+          <SiGmail className={classes.icons} />
+        </a>
+        <a href="https://medium.com/@nikhilkarve007">
+          <BsMedium className={classes.icons} />
+        </a>
+      </div>
     </div>
   );
 }
